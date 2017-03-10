cf3bc04085b6040d1a1cc391cae434a4b7439d39
Airton <ajcolombini@gmail.com>
2017-03-10 00:47:04 -0300

    done

--- UI/FireBaseHelper.cs
+++ UI/FireBaseHelper.cs
@@ -7,6 +7,7 @@ using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Script.Serialization;
 
 namespace UI
 {
@@ -74,41 +75,28 @@ namespace UI
 
         protected const string BasePath = "https://firesharp.firebaseio.com/";
         protected const string BasePathWithoutSlash = "https://firesharp.firebaseio.com";
-        protected string FirebaseSecret = ConfigurationManager.AppSettings["FireBaseToken"];
-
-        private IFirebaseClient _client;
-
+        protected static string _apiKey = ConfigurationManager.AppSettings["FireBaseToken"];
+     
+        public FirebaseConfig fbConfig = new FirebaseConfig(_apiKey);
         
-        public async void TestFixtureSetUp()
+
+        public async void SetDataToFirebase(string text)
         {
             IFirebaseConfig config = new FirebaseConfig
             {
-                AuthSecret = FirebaseSecret,
-                BasePath = BasePath
+                AuthSecret =  "AIzaSyA3LGLgBNc9mxQEPDqSg9GzqKhsIUtW0lI",
+                BasePath = "https://dhub-4bd37.firebaseio.com/"
             };
-            _client = new FirebaseClient(config); //Uses Newtonsoft.Json Json Serializer
+            IFirebaseClient client;
 
-            var task1 = _client.DeleteAsync("todos");
-            var task2 = _client.DeleteAsync("fakepath");
-
-            await Task.WhenAll(task1, task2);
+            //Initializing FirebaseClient with reference link
+            client = new FirebaseClient(config);
+            Dictionary<string, string> values = new Dictionary<string, string>();
+            values.Add("Name", text);
+            var response = await client.PushAsync("FireSharp/Name/", values);
+            
         }
-
-        protected override void FinalizeSetUp()
-        { }
 
         
-        public void Delete()
-        {
-            _client.Push("manufacturer/push", new ManufacturerRegister
-            {
-                name = "Execute PUSH4GET",
-                priority = 2
-            });
-
-            var response = _client.Delete("todos/push");
-            Assert.NotNull(response);
-        }
-
     }
 }
--- UI/ManufacturerRegister.aspx.cs
+++ UI/ManufacturerRegister.aspx.cs
@@ -19,10 +19,23 @@ namespace UI
 
         protected void btnRegistrar_Click(object sender, EventArgs e)
         {
-           
-           //string _body = FireBaseHelper.JsonModel;
 
-            //FireSharp.Response.PushResponse _push = new FireSharp.Response.PushResponse(_body, System.Net.HttpStatusCode.OK, new System.Net.Http.HttpResponseMessage());
+            //string _body = FireBaseHelper.JsonModel;
+
+            FireBaseHelper _help = new FireBaseHelper();
+
+            ModelObject _model = new ModelObject();
+            _model.manufacturer.name = txtNome.Text;
+            _model.manufacturer.eMail = txtEmail.Text;
+            _model.manufacturer.formalName = txtRazao.Text;
+            _model.manufacturer.id = new Guid();
+            _model.manufacturer.webSite = txtSite.Text;
+
+            _help.SetDataToFirebase(this.textBox.Text);
+
         }
+
+        //FireSharp.Response.PushResponse _push = new FireSharp.Response.PushResponse(_body, System.Net.HttpStatusCode.OK, new System.Net.Http.HttpResponseMessage());
+    }
     }
 }
